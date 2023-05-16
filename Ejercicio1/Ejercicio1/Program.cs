ce9c1acb673d7a990d3aa138ada289eedbd22bf1
= <=>
2023-05-16 16:45:24 +0200

    finished

---
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,9 @@
+﻿
+// ejecicio 1 leer nombre y imprimir por consola el nombre.
+Console.WriteLine("Cual es tu nombre:");
+string name = Console.ReadLine();
+Console.WriteLine("Tu nombre es: " + name + ".");
+
+// ejercicio 2 tomar la hora y  imprimir la por pantalla.
+
+Console.WriteLine(DateTime.Now.ToString());
