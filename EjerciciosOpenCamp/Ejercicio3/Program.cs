41c8b6afa68ab0c1bac724af28c7d74307b7d89f
= <=>
2023-05-17 15:24:15 +0200

    ejercicio 3 completado

---
+++ EjerciciosOpenCamp/Ejercicio3/Program.cs
@@ -0,0 +1,27 @@
+﻿Cliente miCliente = new("juan", "lee", 55532322, "barcelona", "juan89@gmail.com", true);
+Console.WriteLine(miCliente);
+
+
+public struct Cliente
+{
+    string name { get; set; }
+    string lastName { get; set; }
+    int phone { get; set; }
+    string city { get; set; }
+    string email { get; set; }
+    bool  client { get; set; }
+
+    public Cliente( string nombre, string apellido, int telefono, string ciudad, string _email, bool nuevoCliente)
+    {
+        name = nombre;
+
+        lastName = apellido;
+        phone = telefono;
+        city = ciudad;
+        email= _email;
+
+        client = nuevoCliente;
+    }
+
+    public override string ToString() => $"({name}, {lastName}, {phone}, {city}, {email}, {client})";
+}
