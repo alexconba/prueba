8efd89db9015334ad897b417ae130d3050580441
= <=>
2023-05-17 18:09:45 +0200

    modificado

--- EjerciciosOpenCamp/Ejercicio4/Program.cs
+++ EjerciciosOpenCamp/Ejercicio4/Program.cs
@@ -1,35 +1,35 @@
-﻿//// Ejercio 1 While
+﻿// Ejercio 1 While
 
-//Console.WriteLine("que tabla de multiplicar quieres?");
-//string n =Console.ReadLine();
-//int numero = Convert.ToInt32(n);
-//int x = 1;
-//while(x <= 10)
-//{
-//    int result = x * numero;
-//    Console.WriteLine(x + " x " + numero +  " = " +result);
-//    x++;
-//}
+Console.WriteLine("que tabla de multiplicar quieres?");
+string n = Console.ReadLine();
+int numero = Convert.ToInt32(n);
+int x = 1;
+while (x <= 10)
+{
+    int result = x * numero;
+    Console.WriteLine(x + " x " + numero + " = " + result);
+    x++;
+}
 
-//// Ejercicio 2 Do while
+// Ejercicio 2 Do while
 
-//Console.WriteLine("escribe un numero entero del 100 al -100: ");
-//int y = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("escribe un numero entero del 100 al -100: ");
+int y = Convert.ToInt32(Console.ReadLine());
 
-//while (y < 0)
-//{
-//    Console.WriteLine(y + " es negativo");
-//    break;
-//}
-//while(y >0)
-//{
-//    Console.WriteLine(y + " es positivo");
-//    break;
-//} while (y == 0)
-//{
-//    Console.WriteLine(y + " el zero es neutral");
-//    break;
-//}
+while (y < 0)
+{
+    Console.WriteLine(y + " es negativo");
+    break;
+}
+while (y > 0)
+{
+    Console.WriteLine(y + " es positivo");
+    break;
+} while (y == 0)
+{
+    Console.WriteLine(y + " el zero es neutral");
+    break;
+}
 
 // Ejecicio3 For
 Console.WriteLine("dibujemos una figura!");
